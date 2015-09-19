7f1609b601997e8169c0625f5b825aa65c780f10
qiaozhanrong <qiaozhanrong@163.com>
2015-09-19 15:53:48 +0800

    怎么还是不对(╯‵□′)╯︵┻━┻

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 			<?php
 			ConnectDb();
 			if(mysql_num_rows(mysql_query("SHOW TABLES LIKE 'Posts'"))!=1){
-				$sql = "CREATE TABLE Posts(
+				$sqlquery = "CREATE TABLE Posts(
 				PID int AUTO_INCREMENT,
 				PRIMARY KEY(PID),
 				username TINYTEXT,
@@ -35,7 +35,7 @@
 				parent int,
 				replycount int DEFAULT 0
 				)";
-				mysql_query($sql);
+				mysql_query($sqlquery);
 				mysql_query("CREATE CLUSTERED INDEX parent_cindex ON Posts(parent) WITH ALLOW_DUP_ROW");
 				mysql_query("INSERT INTO Posts (username, title, content, parent) VALUES ('system', 'main', '', -1) ");
 				echo "POSTS INIT FINSHED!!!";
@@ -43,10 +43,8 @@
    
 			$results = mysql_fetch_array(mysql_query("SELECT * FROM Posts WHERE parent = 0"));
 			foreach (array_reverse($results) as $result) {
-				if($result!=""){
-					echo "<div class='topic'>$result  . {$result["username"]} <a href='posts.php?p={$result['PID']}' > {$result['title']} </a> <br />
-					<span style='padding: 0 2em;'> </span> {$result['content']} <br /> 回复数： {$result['replycount']}  | 发布时间： {$result['createtime']}</div>";
-				}
+				echo "<div class='topic'>{$result['PID']}. {$result["username"]} <a href='posts.php?p={$result['PID']}' > {$result['title']} </a> <br />
+				<span style='padding: 0 2em;'> </span> {$result['content']} <br /> 回复数： {$result['replycount']}  | 发布时间： {$result['createtime']}</div>";
 			}
 
 			DisconnectDb();
