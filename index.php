6de195263b3b5ce5fb989218ae0715cc3b3f7eed
Null <abc612008@126.com>
2015-09-27 10:08:03 +0800

    +3

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ function getfontcolor($replycount){
 	</div>
 </div>
 <div id="main_right">
-	<a href="flatswitch.php">切换到简约版</a>
+	<a href="flatswitch.php">简约版/正常版切换</a>
 	<br />
 	<div class="box">
 		<?php
