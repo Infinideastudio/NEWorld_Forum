cbe0f48aa0606d3dc947095225f2340b44342ddf
Null <abc612008@users.noreply.github.com>
2015-11-21 10:35:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 			echo "<hr style='clear:both;'/>";
 			//公告栏
 			echo '<div class="txtbox" style="background-color:#fafafa;">
-					<marquee direction="up" style="height:100px;" id=m onmouseout=m.start() onMouseOver=m.stop() scrollamount="2.5" scrolldelay="20">';
+					<marquee direction="up" style="height:100px;" id=m scrollamount="2.5" scrolldelay="20">';
 			$result=mysql_query("SELECT * FROM broadcast");
 			while($row=mysql_fetch_array($result)){
 				echo '<p style="margin:0px 30px;">'.$row['content'].'</p>';
